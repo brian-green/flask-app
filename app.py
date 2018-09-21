c86fa8bb20c9fcb69339d9f3e81e1198dc821f08
Brian Green <dear.briangreen@gmail.com>
2018-09-21 00:19:53 -0700

    Added more routes

--- app.py
+++ app.py
@@ -5,5 +5,15 @@ app = Flask(__name__)
 def hello_world():
     return "Hello Brian"
 
+@app.route('/hithere')
+def hi_there_everyone():
+    return "I just hit /hithere"
+
+@app.route('/bye')
+def bye():
+    c = 2*534
+    s = str(c)
+    return "bye " + s
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
