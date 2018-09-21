fcb88e8999579685520e32abf3f409753b38698f
Brian Green <dear.briangreen@gmail.com>
2018-09-21 01:11:13 -0700

    Added POST handling

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
-from flask import Flask
+# Note new modules!
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
+#127.0.0.1:5000/
 @app.route('/')
 def hello_world():
     return "Hello Brian"
 
 @app.route('/hithere')
 def hi_there_everyone():
-    return "I just hit /hithere"
+    return "I just hit /hithere!"
+
+@app.route('/add_two_num', methods=["POST","GET"])
+def add_two_nums():
+    # Get x,y from the post data
+    data = request.get_json()
+    x = data["x"]
+    y = data["y"]
+
+    # Add x,y
+    z = x + y
+
+    # Prepare JSON
+    json = {"z":z}
+
+    # Return JSON to client
+    return jsonify(json)
 
 @app.route('/bye')
 def bye():
